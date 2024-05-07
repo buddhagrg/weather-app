0361ce86f707e055b9d5bc14c6e25ae721983947
Buddha Gurung <16762602+buddhagrg@users.noreply.github.com>
2024-05-07 22:19:44 +0545

    remove unused imports

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,10 +1,7 @@
 import '@testing-library/jest-dom';
-import { beforeAll, afterAll, afterEach, vi } from 'vitest';
+import { beforeAll, afterAll, afterEach } from 'vitest';
 import { cleanup } from '@testing-library/react';
 import { server } from './src/mocks/server';
-import { setupGeoLocation } from './src/utils/setup-geolocation';
-
-// setupGeoLocation({ error: null });
 
 beforeAll(() => {
     server.listen();
