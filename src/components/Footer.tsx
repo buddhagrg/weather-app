3b4764de479f36d8f6c5bff6b435bec0a4ef6cf3
Buddha Gurung <16762602+buddhagrg@users.noreply.github.com>
2024-05-20 22:01:29 +0545

    include project github link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,8 @@ export const Footer: React.FC = () => {
     return (
         <footer className="my-5 text-center">
             Powered by <a href="https://openweathermap.org/">https://openweathermap.org/</a>
+            <br />
+            View code in <a href="https://github.com/buddhagrg/weather-app">Github</a>
         </footer>
     );
 }
